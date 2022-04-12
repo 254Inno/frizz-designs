ef0340951ddd86db9d6020a62dd9046740c7e02d
254Inno <innocentnyakangi@gmail.com>
2022-04-12 00:33:15 -0700

    functionality
    added a toggle in the nav bar using js instead of hover:psudo class from css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+const navigationToggle = document.querySelector(".header__nav--menu");
+const navFull = document.querySelector(".header__nav-full");
+const iconsToggle = document.querySelectorAll(".icon-nav");
+const navLinks = document.querySelectorAll(".header__nav--link");
+
+//Navigation
+
+navigationToggle.addEventListener("click", function () {
+  navFull.classList.toggle("openNav");
+  iconsToggle.forEach((icon) => icon.classList.toggle("icon-visible"));
+  navLinks.forEach((link) => link.classList.toggle("hide-nav-links"));
+});
